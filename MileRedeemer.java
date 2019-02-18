e668f328d2934cc26797ff532cf7da4cee8ec47e
sampiecz <samuel.piecz@gmail.com>
2019-02-18 01:33:21 -0600

    Started on reading input file.

--- MileRedeemer.java
+++ MileRedeemer.java
@@ -1,25 +1,48 @@
 import java.util.Scanner;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MileRedeemer
 {
-    private Destination destinations;
-    private int remainingMiles;
+	private int miles;
+	private String destinationRecord;
+	private Destination destination;
+	private ArrayList<Destination> destinationList = NewArrayList<Destination>();
 
+	// Fixed length array, won't be used until you do the copy to method
+	private Destination[] destinationArray;
 
-    public void readDestinations(Scanner fileScanner)
-    {
-		// Create a scanner
-		Scanner inputScan = new Scanner(System.in);
+	public void readDestinations(Scanner fileName) throws IOException
+	{
+		String destinationRecord;
 
-		// Get input from user
-		System.out.print(message);
+		String [] subStrings;
 
-        // Open file in Java
+		while (fileName.hasNext())
+		{
+			destinationRecord = fileName.nextLine();
+			subStrings = destinationRecord.split(";", 5);
+			destination = new Destination();
 
-    }
+			destination.setCityName(subStrings[0]);
+			destination.setNormalMiles(Integer.parseInt(subStrings[1]));
+			destination.setSaverMiles(Integer.parseInt(subStrings[2]));
+			destination.setUpgradeMiles(Integer.parseInt(subStrings[3]));
+
+			subStrings = subStrings[4].split("-", 2);
+
+			destination.setBeginMonth(Integer.parseInt(subStrings[0]));
+			destination.setEndMonth(Integer.parseInt(subStrings[1]));
+		}
+
+        // Need to sort array in descending order by normal mileage
+	}
+
 
     public String[] getCityNames()
     {
+
     }
 
     public String[] redeemMiles(int miles, int month)
